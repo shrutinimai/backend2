5a02faefe9b2680f82ded9b45add7ba7d8caeac9
shrutinimai <nimaishruti@gmail.com>
2025-02-07 18:54:49 +0530

    debugger

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const http = require('http');
-const routes=require("./routes")
-const server=http.createServer(routes);
-server.listen(3000,()=>{
-    console.log("server is running");
-    });
+
+const server = http.createServer((req, res) => {
+  console.log('Request received:', req.url); 
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Welcome to the homepage!');
+  } else if (req.url === '/about') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Welcome to the about page!');
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Page not found!');
+  }
+});
+
+server.listen(3000, () => {
+  console.log('Server running at http://localhost:3000/');
+});
